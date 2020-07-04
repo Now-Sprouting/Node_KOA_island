384cb366e44778d5f53bb806cccd4e4f69f584f2
Now_Sprouting <15542456716@163.com>
2020-07-04 09:32:16 +0800

    基本路由,错误处理搭建

---
+++ app/api/v1/book.js
@@ -0,0 +1,6 @@
+const Router = require('koa-router')
+const router = new Router()
+router.get('/v1/book',(ctx,next)=>{
+    ctx.body={name:'book'}
+})
+module.exports = router
---
+++ core/init.js
@@ -0,0 +1,24 @@
+const Router = require('koa-router')
+const requireDirectory = require('require-directory')
+
+class InitManager {
+    static initCore(app){
+        // 入口方法
+        InitManager.app = app
+        InitManager.initLoadRouters()
+    }
+    // require-directory模块解决频繁导入(require)和注册(use)的问题
+    static initLoadRouters(){
+        // 绝对路径 process.cwd()为文件加存放的主机路径
+        const apiDirectory = `${process.cwd()}/app/api`
+        requireDirectory(module,apiDirectory,{
+            visit:whenLoadModule
+        })
+        function whenLoadModule(obj){
+            if(obj instanceof Router){
+                InitManager.app.use(obj.routes())
+            }
+        }
+    }
+}
+module.exports = InitManager
